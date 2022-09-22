8afc2cbeb0b626c8afb1456018ea708516728ee9
Ella Morgan <ellamorgan112@gmail.com>
2022-09-22 15:57:29 -0400

    val loaders

--- pddl_vis/pddl/pddl_dataset.py
+++ pddl_vis/pddl/pddl_dataset.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Optional
 from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 from macq.generate.pddl import StateEnumerator
@@ -10,6 +10,44 @@ import numpy as np
 
 
 class PDDLDataset(Dataset):
+
+    def __init__(
+        self,
+        generator: StateEnumerator,
+        vis: Callable,
+        n_samples: int,
+        img_size: tuple,
+        train: bool = True,
+    ) -> None:
+
+        self.states = [generator.tarski_state_to_macq(state) for state in generator.graph.nodes]
+        self.data = [[vis(Step(state, None, 0), size=img_size) for _ in range(n_samples)] for state in self.states]
+        self.targets = list(range(len(self.data)))
+        self.n_states = len(self.data)
+        self.n_samples = n_samples
+        self.n_pairs = math.comb(n_samples, 2)
+        self.train = train
+    
+    def __getitem__(self, index):
+        if self.train:
+            if index >= self.n_states * self.n_pairs:
+                raise IndexError("Dataset index out of range")
+            state_ind = index // self.n_pairs
+            p1, p2 = get_combination(index % self.n_pairs, self.n_samples, self.n_pairs)
+            return index, self.data[state_ind][p1], self.data[state_ind][p2], state_ind
+        else:
+            if index >= self.n_states * self.n_samples:
+                raise IndexError("Dataset index out of range")
+            state_ind = index // self.n_samples
+            sample_ind = index % self.n_samples
+            return self.data[state_ind][sample_ind], state_ind
+    
+    def __len__(self):
+        return self.n_states * self.n_pairs
+
+
+
+class PDDLTrainDataset(Dataset):
 
     def __init__(
         self,
@@ -37,16 +75,33 @@ class PDDLDataset(Dataset):
         return self.n_states * self.n_pairs
 
 
+class PDDLTestDataset(Dataset):
+
+    def __init__(
+        self,
+        generator: StateEnumerator,
+
+    )
+
+
+
 def prepare_dataloader(
-    train_dataset: Dataset, batch_size: int, num_workers: int
+    train_dataset: Dataset,
+    val_dataset: Optional[Dataset] = None,
+    test_dataset: Optional[Dataset] = None,
+    batch_size: int = 64, 
+    num_workers: int = 4,
 ) -> DataLoader:
 
-    def collate_fn(data):
+    def train_collate(data):
         '''
         data: list where each element is [index, img1, img2, state_ind]
         '''
         data = list(zip(*data))
         data = [torch.tensor(data[0]), [torch.tensor(np.array(img)).float() for img in data[1:-1]], torch.tensor(data[-1])]
+        return data
+    
+    def test_collate(data):
         return data
 
     train_loader = DataLoader(
@@ -56,6 +111,31 @@ def prepare_dataloader(
         num_workers=num_workers,
         pin_memory=True,
         drop_last=True,
-        collate_fn=collate_fn,
+        collate_fn=train_collate,
     )
-    return train_loader
+
+    loaders = [train_loader]
+
+    if val_dataset is not None:
+        val_loader = DataLoader(
+            val_dataset,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            pin_memory=True,
+            drop_last=True,
+            collate_fn=test_collate,
+        )
+        loaders.append(val_loader)
+    
+    if test_dataset is not None:
+        test_loader = DataLoader(
+            test_dataset,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            pin_memory=True,
+            drop_last=True,
+            collate_fn=test_collate,
+        )
+        loaders.append(test_loader)
+
+    return loaders
